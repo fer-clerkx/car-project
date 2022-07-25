52b79933b2ae82dc6f76f6d4216c11712cce5080
Fer Clerkx <f.clerkx@student.fontys.nl>
2022-07-26 00:41:57 +0200

    Removed and combined addition files, added README.md

--- motor.c
+++ motor.c
@@ -3,7 +3,6 @@
 #include <errno.h>
 #include <stdio.h>
 #include "IPC.h"
-#include "PID.h"
 #include <unistd.h>
 #include <stdbool.h>
 #include <math.h>
@@ -13,6 +12,33 @@
 #define PWM_Pin 27
 #define PWM_Start 13
 #define PWM_Range 200
+
+#define ENCA 21
+#define ENCB 26
+#define IN1 25
+#define IN2 22
+#define MOTOR_PWM 23
+
+float set_speed = 10;
+const float kp = 50;
+const float ki = 25;
+//const float kd = 0;
+
+volatile int pos_i = 0;
+volatile int posPrev = 0;
+volatile long prevT = 0;
+volatile float v1 = 0;
+volatile float u = 0;
+
+float eIntegral = 0;
+//float eDerivative = 0;
+float prevError = 0;
+
+long outT = 0;
+
+void motorInit();
+void setMotor(int dir, int pwmVal);
+void readEncoder (void);
 
 int error;
 char filePath[] = "/tmp/FIFO_Sensors";
